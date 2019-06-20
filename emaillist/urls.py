b74109bdf73b36389d96d3cc3fb0b69c1b45b973
MaximSungmo <ksm5318@naver.com>
2019-06-20 11:44:56 +0900

    mapping, model 생성

--- emaillist/urls.py
+++ emaillist/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
-import helloworld.views as helloworld_views
+import emaillist.views as emaillist_views
 
 urlpatterns = [
-    path('emaillist/', include('emaillist.urls')),
-    path('helloworld/', include('helloworld.urls')),
-    path('admin/', admin.site.urls)
+    path('', emaillist_views.index),
+    path('form', emaillist_views.form)
+
 ]
--- python_ch3/settings.py
+++ python_ch3/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'helloworld',
+    'emaillist',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
